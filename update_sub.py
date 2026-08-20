b820cdee2e379a49a028c68dc0fcff896e57bbe5
poryiar13 <poryiarahmani@gmail.com>
2026-08-20 13:32:02 +0330

    Update update_sub.py

--- update_sub.py
+++ update_sub.py
@@ -25,7 +25,7 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import quote
 
-CHANNEL = "configraygan"
+CHANNEL = "MARAMBASHI"
 TARGET_CONFIGS = 30
 MAX_PAGES = 25          # safety cap on how far back in history to look
 
